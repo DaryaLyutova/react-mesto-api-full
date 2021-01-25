5b1741d4c977e749befe1ddbb5f2a9ecfde78a47
Darya Lutuva <lutowa.darya@yandex.ru>
2021-01-25 12:39:54 +0300

    url в api

--- backend/routes/errorUrl.js
+++ backend/routes/errorUrl.js
@@ -1,10 +1,11 @@
 /* eslint-disable quotes */
 /* eslint-disable quote-props */
 const router = require('express').Router();
+const NotFoundError = require('../errors/not-found-err');
 
 // eslint-disable-next-line arrow-body-style
-router.get('/*', (req, res) => {
-  return res.status(404).send({ 'message': "Запрашиваемый ресурс не найден" });
+router.get('/*', (req, res, next) => {
+  next(new NotFoundError({ 'message': "Запрашиваемый ресурс не найден" }));
 });
 
 module.exports = router;
